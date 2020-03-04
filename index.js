15b49bc79f41cfc6a83ea8fa10b4a61ff0cf41cb
Tashalia Nair <33638169+novacaine98@users.noreply.github.com>
2020-03-04 10:23:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const btn = document.querySelector('button');
 
-function random(number){
-    return Math.floor(Math.random() * (number+1));
-}
+function bgChange() {
+  const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  document.body.style.backgroundColor = rndCol;
+}   
 
-btn.onclick = function (){
-    const rndCol = 'rg('+ random (255) + ',' + random (255) + random(255) + ')';
-    document.body.style.backgroundColor = rndCol;
-}
+btn.addEventListener('click', bgChange);
