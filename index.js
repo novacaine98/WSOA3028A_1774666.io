04e6b4d365a9c79a44af25d1738196ddad78a9f6
Tashalia Nair <33638169+novacaine98@users.noreply.github.com>
2020-03-04 10:31:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function random(number) {
   return Math.floor(Math.random() * (number+1));
 }
 
-btn.onclick = function() {
+btn.onclick = function()
+{
   const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
   document.body.style.backgroundColor = rndCol;
 }
