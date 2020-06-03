cf8dcbf460566ecc48afffd5f55bc51d671d1f43
Tashalia Nair <33638169+novacaine98@users.noreply.github.com>
2020-06-03 22:22:56 +0200

    back button fixed and trying to get buttons to match the rest of the navigation links

--- html/Javascript/index.js
+++ html/Javascript/index.js
@@ -1,15 +1,3 @@
-const btn = document.querySelector('button');
-
-function random(number) {
-  return Math.floor(Math.random() * (number+1));
-}
-
-btn.onclick = function()
-{
-  const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-  document.body.style.backgroundColor = rndCol;
-}
-
 
 document.getElementById('go-back').addEventListener('click', () => {history.back(); });
 
