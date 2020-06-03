9a4e2f99a6c2fa587237753d2a5598e06269785f
Tashalia Nair <33638169+novacaine98@users.noreply.github.com>
2020-06-03 22:16:37 +0200

    css editing, adding the buttons/javascript

--- html/Javascript/index.js
+++ html/Javascript/index.js
@@ -9,3 +9,8 @@ btn.onclick = function()
   const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
   document.body.style.backgroundColor = rndCol;
 }
+
+
+document.getElementById('go-back').addEventListener('click', () => {history.back(); });
+
+document.getElementById('go-forward').addEventListener('click', e => {window.history.forward(); });
