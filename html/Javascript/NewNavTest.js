4d33356b6e352835dcfb806bf035f88488b7decf
Tashalia Nair <33638169+novacaine98@users.noreply.github.com>
2020-06-27 19:56:50 +0200

    new navigation test, plain responsive top bar nav

--- html/Javascript/NewNavTest.js
+++ html/Javascript/NewNavTest.js
@@ -1,10 +1,9 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction(){
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementById("topnav").style.padding = "30px 10px";
+function myFunction(){
+    var x = document.getElementById("myTopnav");
+    if(x.className === "topnav"){
+        x.className += " responsive";
     }
-    else {
-        document.getElementById ("topmav".style.padding = "80px 10px");
+    else{
+        x.className = "topnav";
     }
 }
