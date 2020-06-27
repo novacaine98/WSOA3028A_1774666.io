e44f1f9709fd9f448ef30a9254c21c3e9e7d8083
Tashalia Nair <33638169+novacaine98@users.noreply.github.com>
2020-06-27 20:37:08 +0200

    ANOTHER nav bar test

--- html/Javascript/NewNavTest.js
+++ html/Javascript/NewNavTest.js
@@ -1,8 +1,6 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
